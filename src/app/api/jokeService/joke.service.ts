e0a34597ab84e6443e9bfffc116e0be6db233540
Arthur <arthur.cuney@diiage.org>
2021-12-13 13:54:52 +0100

    feat(partie 1): Service

---
+++ src/app/api/jokeService/joke.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/models/post.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokeService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getJokes(): any {
+    this.httpClient.get('http://localhost:3001/posts').subscribe(
+      value => {
+        return value;
+      });
+  }
+}
